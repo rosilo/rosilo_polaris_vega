40302daa26e89787026fdcff95b5281153121ad2
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2020-06-24 22:18:55 -0700

    Updating license headers

--- include/rosilo_polaris_vega/rosilo_polaris_vega_interface.h
+++ include/rosilo_polaris_vega/rosilo_polaris_vega_interface.h
@@ -1,32 +1,21 @@
 #pragma once
 /*
-# Copyright (c) 2016-2020 Murilo Marques Marinho
+# Copyright (c) 2012-2020 Murilo Marques Marinho
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
+#    This file is part of rosilo_polaris_vega.
 #
-#     - Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     - Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     - Neither the name of the Mitsuishi Sugita Laboratory (NML) nor the
-#       names of its contributors may be used to endorse or promote products
-#       derived from this software without specific prior written permission.
+#    rosilo_polaris_vega is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License LGPL as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+#    rosilo_polaris_vega is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Lesser General Public License LGPL for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License LGPL along with this program.
-# If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with rosilo_polaris_vega.  If not, see <https://www.gnu.org/licenses/>.
 #
 # ################################################################
 #
